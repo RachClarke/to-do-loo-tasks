856446be61c14e547dcef3d61eefb11f15c7b6a3
Rachel Clarke <clarkerachel21@gmail.com>
2020-05-04 19:04:11 +0100

    wrote a test for signed in user creating a task

---
+++ test/system/tasks_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+require 'application_system_test_case'
+
+class TaskTest < ApplicationSystemTestCase
+  # include Warden::Test::Helpers
+
+  test 'lets a signed in user create a task' do
+    login_as users(:Rory)
+    visit '/tasks/new'
+
+    fill_in 'Name', with: 'Feed the dog'
+
+    click_on 'Create Task'
+
+    assert_equal root_path, page.current_path
+    assert_text 'Feed the dog'
+  end
+end
